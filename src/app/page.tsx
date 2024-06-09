6ec028d362663d4154d84b883d23a4fe1f336105
James Rivera <112741353+JamesRiveran@users.noreply.github.com>
2024-06-09 14:33:50 -0600

    [FIX] Bienvenida y flujo de ventanas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,36 @@
 "use client";
-import { ChangeEvent, useState } from "react";
-import Image from "next/image";
-import {
-  sendFileToDestination,
-  getImageFromDestination,
-} from "./hooks/hookSaveImage";
+import { useState } from "react";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Map from "@/components/Map";
 import Login from "@/components/Login";
+import Welcome from "@/components/Welcome";
+
 export default function Home() {
   const [showMap, setShowMap] = useState(false);
+  const [showLoginComponent, setShowLoginComponent] = useState(false);
 
   const handleLogin = () => {
     setShowMap(true);
+    setShowLoginComponent(true);
+  };
+
+  const handleShowLogin = () => {
+    setShowLoginComponent(true);
   };
 
   return (
     <>
-      <Header />
-      {showMap ? <Map /> : <Login onLogin={handleLogin} />}
+      <Header handleShowLogin={handleShowLogin} />
+      {showLoginComponent ? (
+        showMap ? (
+          <Map />
+        ) : (
+          <Login onLogin={handleLogin} />
+        )
+      ) : (
+        <Welcome />
+      )}
       <Footer />
     </>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,19 @@
-/**
- * v0 by Vercel.
- * @see https://v0.dev/t/nbJGWjpH0Fw
- * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
- */
 import { Button } from "@/components/ui/button"
 
-export default function Component() {
+export default function Component({ handleShowLogin }: { handleShowLogin: () => void }) {
   return (
     <header className="w-full bg-[#30363d] py-6 px-6 md:px-8 lg:px-10">
       <div className="container mx-auto flex flex-col items-center justify-between sm:flex-row">
         <div className="flex flex-col items-center">
           <h1 className="text-4xl font-bold text-[#693CA5]">Geo-Snap</h1>
           <span className="text-white font-medium text-lg">
-            Captura momentos inolvidables.
+            Captura momentos inolvidables
           </span>
         </div>
         <Button
           variant="default"
           className="bg-[#693CA5] text-white hover:bg-[#5a3088] focus:outline-none focus:ring-2 focus:ring-[#693CA5] focus:ring-opacity-50 mt-4 sm:mt-0 px-6 py-3"
+          onClick={handleShowLogin}
         >
           Empezar
         </Button>
