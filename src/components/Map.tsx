cc622e86dbb65661cd91298376f4b8b837430397
Ghost-Red <raulbejarano2002@gmail.com>
2024-06-10 16:21:30 -0600

    Implementacion mapa

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,46 +1,51 @@
-import React from 'react';
-import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
-
+import React, { useEffect, useState } from "react"
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api"
+import getPhotos from "@/lib/getPhotos"
+import { Photo } from "@/types/type"
+import useGeolocation from "@/hooks/useGeolocation"
 
 const mapContainerStyle = {
-  width: '100vw',
-  height: '100vh',
-};
-const center = {
-  lat: 9.380168965718807,
-  lng: -83.69172815398791,
-};
-
-const other = {
-    lat:  9.38631621875457,
-    lng: -83.70001588351072,
-  };
+  width: "100vw",
+  height: "100vh",
+}
 
 const Map = () => {
+  const [photos, setPhotos] = useState<Photo[]>([])
+  const { coordinates } = useGeolocation()
+
+  useEffect(() => {
+    const fetch = async () => {
+      const recentPhotos = await getPhotos()
+      setPhotos(recentPhotos)
+    }
+    fetch
+  }, [])
+
   const { isLoaded, loadError } = useLoadScript({
-    googleMapsApiKey: 'AIzaSyD_cXy0RgcLC5VAjc1iAgVCfIjW0YLU_KM'
-  });
+    googleMapsApiKey: "AIzaSyD_cXy0RgcLC5VAjc1iAgVCfIjW0YLU_KM",
+  })
 
   if (loadError) {
-    return <div>Error loading maps</div>;
+    return <div>Error loading maps</div>
   }
 
   if (!isLoaded) {
-    return <div>Loading Maps</div>;
+    return <div>Loading Maps</div>
   }
 
   return (
     <div>
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
-        zoom={10}
-        center={center}
+        zoom={15}
+        center={{ lat: coordinates.latitude ?? 9.380168965718807, lng: coordinates.longitude ?? -83.69172815398791 }}
       >
-        <Marker position={center} />
-        <Marker position={other} />
+        {photos.map((photo) => (
+          <Marker key={photo.photoID} position={{ lat: Number(photo.latitude), lng: Number(photo.longitude)}} />
+        ))}
       </GoogleMap>
     </div>
-  );
-};
+  )
+}
 
-export default Map;
+export default Map
---
+++ src/types/type.d.ts
@@ -0,0 +1,10 @@
+export interface Photo {
+  likes: number;
+  userID: string;
+  longitude: string;
+  userName: string;
+  uploadTime: Date;
+  photoID: string;
+  latitude: string;
+  photoURL: string;
+}
